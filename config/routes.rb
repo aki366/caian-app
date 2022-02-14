4a4ae9cd68fa7803a3ed10db9718e6793995fc98
aki366 <free_a.d_ad@outlook.jp>
2022-02-14 21:16:03 +0900

    0214_投稿ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#top' 
-  get 'posts/index' => "posts#index"
   get "top" => "home#top"
   get "about" => "home#about"
+  get 'posts/index' => "posts#index"
+  get "posts/:id" => "posts#show"
 end
