f64fa3bb05dd1f861f4be599cecd24a85973a026
aki366 <free_a.d_ad@outlook.jp>
2022-02-15 01:11:08 +0900

    0215_投稿_編集_削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "about" => "home#about"
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 end
