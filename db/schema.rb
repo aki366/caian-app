a478468a04584fd12b16056d719a45e0e97d084d
aki366 <free_a.d_ad@outlook.jp>
2022-02-19 06:32:31 +0900

    0219_テーブル再設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_004918) do
+ActiveRecord::Schema.define(version: 2022_02_17_004918) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.string "user_image"
+    t.string "profile"
+    t.string "career"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
