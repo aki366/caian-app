b7bd875ed8af46ba834a8abcf38b779eceeff796
aki366 <free_a.d_ad@outlook.jp>
2022-02-18 23:39:45 +0900

    0218_usersテーブル設定

--- db/migrate/20220218011932_create_users.rb
+++ db/migrate/20220218011932_create_users.rb
@@ -3,6 +3,10 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :password
+      t.string :user_image
+      t.string :profile
+      t.string :career
 
       t.timestamps
     end
