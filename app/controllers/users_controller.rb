5295e44a080b37b27070363ae1f726161009151a
aki366 <free_a.d_ad@outlook.jp>
2022-02-25 05:48:06 +0900

    0225_ユーザーが存在しない時、する時の処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,5 +51,15 @@ class UsersController < ApplicationController
   def login_form
     # @user = User.login_form
   end
+  
+  def login
+    @user = User.find_by(email: params[:email],password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to ("/posts/index")
+    else
+      render("users/login_form")
+    end
+  end
 
 end
