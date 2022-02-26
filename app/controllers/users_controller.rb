504ace0374f121f2b53da63ff8273d4c27e3ac02
aki366 <free_a.d_ad@outlook.jp>
2022-02-26 14:06:15 +0900

    0226_ログインユーザーの情報を保持

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,9 +55,13 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email],password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to ("/posts/index")
     else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
       render("users/login_form")
     end
   end
