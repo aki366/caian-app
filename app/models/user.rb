be0626af8cf8547a4a4509457fb12746f79fa551
aki366 <free_a.d_ad@outlook.jp>
2022-03-23 22:38:18 +0900

    0323_アソシエーションについてコメント追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
 
   # Postモデルとのアソシエーションを作成
   # Userのインスタンスはpostsメソッドを使えるようになる
+  # UserモデルはPostモデルをたくさん持っていて（1対多の関係）
+  # モデル同士の関係を表現するものなので、二つでセットそのため、app/models/post.rbファイルではbelongs_to :userと定義している。
   # @user.posts
   has_many :posts
 
