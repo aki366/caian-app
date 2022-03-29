009964e2f8bd51a215185d986f0f3b9dfc33d9ba
aki366 <free_a.d_ad@outlook.jp>
2022-03-29 22:28:57 +0900

    0329_db:migrate実施 #11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_005348) do
+ActiveRecord::Schema.define(version: 2022_03_29_131748) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2022_03_15_005348) do
     t.string "title"
     t.string "image_id"
     t.string "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
