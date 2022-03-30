848f4f451fdb1e6142c4d59101b35979b26b9f96
aki366 <free_a.d_ad@outlook.jp>
2022-03-30 20:06:00 +0900

    0330_「いいね！」ボタン追加 #11

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,11 @@ Rails.application.routes.draw do
   get "top" => "home#top"
   get "about" => "home#about"
 
-  # get "posts/index" => "posts#index"
-  # get "posts/new" => "posts#new"
-  # get "posts/:id" => "posts#show"
-  # get "posts/:id/edit" => "posts#edit"
-  # post "posts/create" => "posts#create"
-  # post "posts/:id/update" => "posts#update"
-  # post "posts/:id/destroy" => "posts#destroy"
-  # 上記の7つのメソッドは下記にまとめられる
   resources :posts
-  
-  # get 'users/index'
-  # post "users/create" => "users#create"
-  # get "users/index" => "users#index"
-  # get "users/:id" => "users#show"
-  # get "users/:id/edit" => "users#edit" 
-  # post "users/:id/update" => "users#update"
-  # 上記のメソッドは下記にまとめられる（7つのメソッドのうち、destroy以外)
   resources :users, except: [:destroy]
+  resources :likes
+  post "likes/:post_id/create", to: "likes#create"
+  post "likes/:post_id/destroy", to: "likes#destroy"
 
   get "signup" => "users#new"
   get "login" => "users#login_form"
