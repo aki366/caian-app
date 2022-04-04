3b6618a0f7f1fff1f268a5f7ca4780a9eb7dcffc
aki366 <free_a.d_ad@outlook.jp>
2022-04-04 21:20:14 +0900

    0404_コメントの削除機能実装 #39

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,15 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @current_user.id == @comment.user.id
+      @comment.destroy
+      redirect_to post_path(@post.id)
+    end
+  end
+
   private
 
     def comment_params
