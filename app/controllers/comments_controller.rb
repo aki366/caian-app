5c7917b889f261fc5f2abd724619df3b8de72e51
aki366 <free_a.d_ad@outlook.jp>
2022-04-04 18:01:28 +0900

    0404_RuboCop_エラーの修正 #40

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,19 @@
 class CommentsController < ApplicationController
-
   def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
-    # @comment = @current_user.comments.new(comment_params)
     @comment.user_id = @current_user.id
     @comment.post_id = @post.id
     if @comment.save
       flash[:notice] = "コメントを投稿しました"
       redirect_to post_path(@post.id)
-    # else
-    #   render post_path(@post.id)
     end
   end
-  
+
   private
 
-  def comment_params
-    #formから渡ってきたパラメーターのうちcomment_textだけを許容する
-    params.require(:comment).permit(:comment_text)
-  end
+    def comment_params
+      # formから渡ってきたパラメーターのうちcomment_textだけを許容する
+      params.require(:comment).permit(:comment_text)
+    end
 end
