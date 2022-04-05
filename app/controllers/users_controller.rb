b68a9b1a51b128c9446eafb34a343f638549d512
aki366 <free_a.d_ad@outlook.jp>
2022-04-05 22:36:35 +0900

    0405_新規ユーザー登録時に画像を設定しても反映されない #48

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params.merge(user_image: "guest_user_icon.png"))
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
@@ -100,7 +100,7 @@ class UsersController < ApplicationController
 
     def user_params
       # ストロングパラメーター
-      params.permit(:name, :email, :password)
+      params.permit(:name, :user_image, :email, :password)
     end
 
     def login_params
