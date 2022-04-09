5f9bf3f4be78357b6f821d746ae2a4f1cebc3fcc
aki366 <free_a.d_ad@outlook.jp>
2022-04-09 18:01:36 +0900

    0409_DMの投稿機能実装済 #13

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def create
+    @room = Room.find(params[:room_id])
+    @message = Message.new(message_params)
+    @message.user_id = @current_user.id
+    @message.room_id = @room.id
+    if @message.save
+      flash[:notice] = "投稿しました"
+      redirect_to room_path(@room.id)
+    end
+  end
+
+  private
+
+    def message_params
+      # formから渡ってきたパラメーターのうちmessage_textだけを許容する
+      params.require(:message).permit(:message_text)
+    end
 end
