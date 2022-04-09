e234ffd90ef5c83a600c4b47bf3f086447229a44
aki366 <free_a.d_ad@outlook.jp>
2022-04-09 18:19:40 +0900

    0409_DMの文字数制限を140から1000へ変更 #13

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  validates :message_text, {presence: true, length: {maximum: 140}}
+  validates :message_text, {presence: true, length: {maximum: 1000}}
 
   belongs_to :room
   belongs_to :user
