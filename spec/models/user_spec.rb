08b36de6952bfb4d82ef69baea1293a24d695d4a
aki366 <96239914+aki366@users.noreply.github.com>
2022-04-21 01:32:43 +0900

    0421_userモデル_test_修正01 #3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,55 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  it '名前がない場合無効' do
-    @user = FactoryBot.build(:user, name: nil)
-    @user.valid?
-    expect(@user.errors.full_messages).to include('Nameを入力してください')
+  before do
+    @user = FactoryBot.build(:user)
   end
 
-  it 'メールアドレスがない場合無効' do
-    @user = FactoryBot.build(:user, email: nil)
-    @user.valid?
-    expect(@user.errors.full_messages).to include('Emailを入力してください')
-  end
+  describe '新規ユーザー登録' do
+    context '新規ユーザー登録がうまくいくとき' do
+      it '名前が30文字以下の場合' do
+        @user.name = 'a' * 30
+        @user.valid?
+      end
+      it 'メールアドレスの大文字と小文字が違う場合' do
+        @user = FactoryBot.create(:user)
+        @user2 = FactoryBot.build(:user, email: @user.email.upcase)
+        @user2.valid?
+        expect(@user2).to be_valid
+      end
+    end
 
-  it 'パスワードがない場合無効' do
-    @user = FactoryBot.build(:user, password: nil)
-    @user.valid?
-    expect(@user.errors.full_messages).to include('Passwordを入力してください')
-  end
-
-  it '名前が30文字以下の場合有効' do
-    text = 'a' * 30
-    @user = FactoryBot.build(:user, name: text)
-    @user.valid?
-    expect(@user).to be_valid
-  end
-
-  it '名前が31文字以上の場合無効' do
-    text = 'a' * 31
-    @user = FactoryBot.build(:user, name: text)
-    @user.valid?
-    expect(@user.errors.full_messages).to include('Nameは30文字以内で入力してください')
-  end
-
-  it '同じメールアドレスの場合無効' do
-    @user = FactoryBot.create(:user)
-    @user2 = FactoryBot.build(:user, email: @user.email)
-    @user2.valid?
-    expect(@user2.errors.full_messages).to include('Emailはすでに存在します')
-  end
-
-  it 'メールアドレスの大文字と小文字が違う場合有効' do
-    @user = FactoryBot.create(:user)
-    @user2 = FactoryBot.build(:user, email: @user.email.upcase)
-    @user2.valid?
-    expect(@user2).to be_valid
-  end
-
-  it 'メールアドレスの形が不正の場合無効' do
-    @user = FactoryBot.build(:user, email: '456-9333')
-    @user.valid?
+    context '新規ユーザー登録がうまくいかないとき' do
+      it '名前がない場合' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Nameを入力してください')
+      end
+      it 'メールアドレスがない場合' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Emailを入力してください')
+      end
+      it 'パスワードがない場合' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Passwordを入力してください')
+      end
+      it '名前が31文字以上の場合' do
+        @user.name = 'a' * 31
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Nameは30文字以内で入力してください')
+      end
+      it '同じメールアドレスの場合' do
+        @user = FactoryBot.create(:user)
+        @user2 = FactoryBot.build(:user, email: @user.email)
+        @user2.valid?
+        expect(@user2.errors.full_messages).to include('Emailはすでに存在します')
+      end
+      it 'メールアドレスの形が不正の場合' do
+        @user.email = '456-9333'
+        @user.valid?
+      end
+    end
   end
 end
