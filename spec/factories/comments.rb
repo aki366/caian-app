950c87c57886cfba698a11cf2fcfb8cc28a49bef
aki366 <96239914+aki366@users.noreply.github.com>
2022-04-21 13:51:17 +0900

    0421_commentモデルのテスト作成 #3

---
+++ spec/factories/comments.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :comment do
+    association :post
+    comment_text { 'テスト投稿' }
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  
+  describe '新規コメント投稿' do
+    context '投稿がうまくいくとき' do
+      it '投稿内容が1文字の場合' do
+        @comment.comment_text = 'a' * 1
+        @comment.valid?
+      end
+      it '内容が1000文字の場合' do
+        @comment.comment_text = 'a' * 1000
+        @comment.valid?
+      end
+    end
+
+    context '投稿がうまくいかないとき' do
+      it '投稿内容がない場合' do
+        @comment.comment_text = ''
+        @comment.valid?
+        # expect(@comment.errors.full_messages).to include('Contentを入力してください')
+      end
+      it '内容が1001文字の場合' do
+        @comment.comment_text = 'a' * 1001
+        @comment.valid?
+        # expect(@comment.errors.full_messages).to include('Contentは1000文字以内で入力してください')
+      end
+    end
+  end
 end
