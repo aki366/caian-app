d6c7243858248d9eb606fe9ad95a1c7cb83be781
aki366 <96239914+aki366@users.noreply.github.com>
2022-04-21 14:12:13 +0900

    0421_testメッセージの統一 #3

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,24 +6,24 @@ RSpec.describe Post, type: :model do
   end
   
   describe '新規投稿' do
-    context '新規投稿がうまくいくとき' do
+    context '投稿がうまくいくとき' do
       it '投稿内容が1文字の場合' do
         @post.content = 'a' * 1
         @post.valid?
       end
-      it '内容が1000文字の場合' do
+      it '投稿内容が1000文字の場合' do
         @post.content = 'a' * 1000
         @post.valid?
       end
     end
 
-    context '新規投稿がうまくいかないとき' do
+    context '投稿がうまくいかないとき' do
       it '投稿内容がない場合' do
         @post.content = ''
         @post.valid?
         expect(@post.errors.full_messages).to include('Contentを入力してください')
       end
-      it '内容が1001文字の場合' do
+      it '投稿内容が1001文字の場合' do
         @post.content = 'a' * 1001
         @post.valid?
         expect(@post.errors.full_messages).to include('Contentは1000文字以内で入力してください')
