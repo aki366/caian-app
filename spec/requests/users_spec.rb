2e24053797acf3c730eb934caac6d7c6a7fe7af8
aki366 <96239914+aki366@users.noreply.github.com>
2022-04-23 20:03:28 +0900

    0423_test_userリクエスト作成_02 #3

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   let(:user) { create(:user) }
-  before do
-    @guest_user = User.create(
-      name: 'ゲストユーザー',
-      email: 'guest@example.com',
-      password: 'password'
-    )
-  end
+  # before do
+  #   @guest_user = User.create(
+  #     name: 'ゲストユーザー',
+  #     email: 'guest@example.com',
+  #     password: 'password'
+  #   )
+  # end
 
   describe 'GET #show' do
     context 'ユーザーが存在するとき' do
@@ -26,95 +26,95 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET #new' do
-    it 'リクエストが成功すること' do
-      get new_user_path
-      expect(response.status).to eq 200
+    context '新規ユーザー登録をするとき' do
+      it 'リクエストが成功すること' do
+        get new_user_path
+        expect(response.status).to eq 200
+      end
     end
   end
 
   describe 'GET #edit' do
     context 'ログイン状態のとき' do
       # before do
-      #   @user = create(:user)
-      #   get edit_user_path
+      #   session[:user_id] = @user.id
       # end
+      it 'リクエストが成功すること' do
+        # get edit_user_path
+        # expect(response.status).to eq 200
+      end
 
-      # it 'リクエストが成功すること' do
-      #   expect(response.status).to eq 200
-      # end
-
-  #     it 'ユーザー名が表示されていること' do
-  #       expect(response.body).to include 'test'
-  #     end
+      it 'ユーザー名が表示されていること' do
+    #     expect(response.body).to include 'test'
+      end
     end
 
-  #   context 'ゲストユーザーの場合' do
+    context 'ゲストユーザーの場合' do
   #     before do
   #       sign_in(@guest_user)
   #       get edit_user_registration_path(@guest_user)
-  #     end
-
-  #     it 'トップページにリダイレクトされること' do
+      # end
+      it 'トップページにリダイレクトされること' do
   #       expect(response.status).to eq 302
   #       expect(response).to redirect_to root_path
-  #     end
-  #   end
-  # end
+      end
+    end
+  end
 
-  # describe 'POST #create' do
-  #   describe 'パラメータが妥当なとき' do
+  describe 'POST #create' do
+    describe 'パラメータが妥当なとき' do
   #     before do
   #       post user_registration_path
   #     end
 
-  #     it 'リクエストが成功すること' do
+      it 'リクエストが成功すること' do
   #       expect(response.status).to eq 200
-  #     end
+      end
 
-  #     it 'ユーザーが登録されること' do
+      it 'ユーザーが登録されること' do
   #       expect(user.name).to eq 'test'
-  #     end
-  #   end
-  # end
+      end
+    end
+  end
 
-  # describe 'PUT #update' do
-  #   context '未ログイン状態のとき' do
-  #     it 'ログインページにリダイレクトされること' do
+  describe 'PUT #update' do
+    context '未ログイン状態のとき' do
+      it 'ログインページにリダイレクトされること' do
   #       patch user_registration_path, params: { user: attributes_for(:user).merge(password: 'testtest') }
   #       expect(response.status).to eq 302
   #       expect(response).to redirect_to new_user_session_path
-  #     end
-  #   end
+      end
+    end
 
-  #   context 'ログイン状態でパラメータが妥当な場合' do
+    context 'ログイン状態でパラメータが妥当な場合' do
   #     before do
   #       sign_in(user)
   #       patch user_registration_path, params: { user: attributes_for(:user).merge(password: 'testtest') }
-  #     end
+      # end
 
-  #     it 'リクエストが成功すること' do
+      it 'リクエストが成功すること' do
   #       expect(response.status).to eq 302
-  #     end
+      end
 
-  #     it 'ユーザー名が更新されること' do
+      it 'ユーザー名が更新されること' do
   #       expect(User.name).not_to eq 'test'
-  #     end
+      end
 
-  #     it 'リダイレクトすること' do
+      it 'リダイレクトすること' do
   #       expect(response).to redirect_to user
-  #     end
-  #   end
+      end
+    end
 
-  #   context 'ゲストユーザーのとき' do
+    context 'ゲストユーザーのとき' do
   #     before do
   #       sign_in(@guest_user)
   #       patch user_registration_path, params: { user: attributes_for(:user).merge(password: 'testtest') }
   #     end
 
-  #     it 'トップページにリダイレクトされること' do
+      it 'トップページにリダイレクトされること' do
   #       expect(response.status).to eq 302
   #       expect(response).to redirect_to root_path
-  #     end
-  #   end
+      end
+    end
   end
 end
