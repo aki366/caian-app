5eb29d67a4012880b678a3ff1d69c17fdaa76920
aki366 <96239914+aki366@users.noreply.github.com>
2022-04-27 15:47:55 +0900

    0427_test_user_update #77

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,15 +12,20 @@ RSpec.describe 'Users Request', type: :request do
   end
 
   describe 'POST #create' do
-    describe 'パラメータが妥当なとき' do
+    context 'パラメータが妥当なとき' do
       before do
-        # post users_path
+        # @user = FactoryBot.build(:user)
+        # @user = FactoryBot.create(:user)
+        # get user_path(@user.id)
       end
       it 'リクエストが成功すること' do
         # expect(response.status).to eq 200
       end
       it 'ユーザーが登録されること' do
         # expect(user.name).to eq 'test'
+      end
+      it 'メッセージが表示されること' do
+        # expect(flash[:notice]).to eq 'ユーザー登録が完了しました'
       end
     end
   end
@@ -32,7 +37,7 @@ RSpec.describe 'Users Request', type: :request do
         .to receive(:session).and_return(user_id: user.id)
         get user_path(user.id)
       end
-      it 'ユーザー詳細画面に遷移すること' do
+      it 'リクエストが成功すること' do
         expect(response.status).to eq 200
       end
       it 'ユーザー名が表示されること' do
@@ -48,64 +53,24 @@ RSpec.describe 'Users Request', type: :request do
           .to receive(:session).and_return(user_id: user.id)
         get edit_user_path(user.id)
       end
-      it '編集画面に遷移すること' do
+      it 'リクエストが成功すること' do
         expect(response.status).to eq 200
       end
       it 'ユーザー名が表示されること' do
         expect(response.body).to include user.name
       end
     end
-
-    context 'ゲストユーザーの場合' do
-  #     before do
-  #       sign_in(@guest_user)
-  #       get edit_user_registration_path(@guest_user)
-      # end
-      it 'トップページにリダイレクトされること' do
-  #       expect(response.status).to eq 302
-  #       expect(response).to redirect_to root_path
-      end
-    end
   end
 
-
   describe 'PUT #update' do
-    context '未ログイン状態のとき' do
-      it 'ログインページにリダイレクトされること' do
-  #       patch user_registration_path, params: { user: attributes_for(:user).merge(password: 'testtest') }
-  #       expect(response.status).to eq 302
-  #       expect(response).to redirect_to new_user_session_path
+    context 'ログイン状態のとき' do
+      before do
+      allow_any_instance_of(ActionDispatch::Request)
+      .to receive(:session).and_return(user_id: user.id)
+      put user_path(user.id)
       end
-    end
-
-    context 'ログイン状態でパラメータが妥当な場合' do
-  #     before do
-  #       sign_in(user)
-  #       patch user_registration_path, params: { user: attributes_for(:user).merge(password: 'testtest') }
-      # end
-
       it 'リクエストが成功すること' do
-  #       expect(response.status).to eq 302
-      end
-
-      it 'ユーザー名が更新されること' do
-  #       expect(User.name).not_to eq 'test'
-      end
-
-      it 'リダイレクトすること' do
-  #       expect(response).to redirect_to user
-      end
-    end
-
-    context 'ゲストユーザーのとき' do
-  #     before do
-  #       sign_in(@guest_user)
-  #       patch user_registration_path, params: { user: attributes_for(:user).merge(password: 'testtest') }
-  #     end
-
-      it 'トップページにリダイレクトされること' do
-  #       expect(response.status).to eq 302
-  #       expect(response).to redirect_to root_path
+        expect(response.status).to eq 200
       end
     end
   end
