914ea0a7206a56702e786bc27eab0f4881074816
aki366 <96239914+aki366@users.noreply.github.com>
2022-05-17 16:20:29 +0900

    0517_posts_indexのN+1問題対処 #106

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :ensure_correct_user,{only:[:edit, :update, :destroy]}
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def show
