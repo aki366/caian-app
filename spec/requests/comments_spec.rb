a1bd605e43f110b6eeae673f705a54c47673bca9
aki366 <96239914+aki366@users.noreply.github.com>
2022-06-27 13:46:17 +0900

    0627_test_'POST #create'_'パラメータが正常なとき'_'コメントが投稿できること' #82

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -3,63 +3,61 @@ require 'rails_helper'
 RSpec.describe "Comments Request", type: :request do
 
   describe 'POST #create' do
+    let!(:user_post) { create(:post) }
+    let!(:user) { create(:user) }
+    let(:user_comment) { create(:comment, post_id: post.id) }
+    include_context 'login_as_user'
     context 'パラメータが正常なとき' do
-      it 'コメントが作成できること' do
+      it 'コメントが投稿できること' do
+        byebug
+        expect do
+          post post_comments_path(user_post.id), params: { comment: {comment_text: "コメントを投稿しました"} }
+
+          # post post_comments_path, params: { comment: attributes_for(:comment) }
+        end.to change(Comment, :count).by(+1)
+        expect(response).to have_http_status(:redirect)
       end
     end
     context 'パラメータが不正なとき' do
-      it 'コメントが作成できないこと' do
+      it 'コメントが投稿できないこと' do
       end
     end
   end
 
-  describe 'GET #edit' do
-    context 'ログインしているとき' do
-      context 'ユーザーが自分の場合' do
-        it 'コメントの編集画面に遷移できること' do
-        end
-      end
-      context 'ユーザーが自分ではない場合' do
-        it 'コメントの編集画面に遷移できないこと' do
-        end
-      end
-    end
-    context 'ログインしていないとき' do
-      it 'コメントの編集画面に遷移できないこと' do
-      end
-    end
+  # describe 'GET #edit' do
+  #   context 'ログインしているとき' do
+  #     context 'ユーザーが自分の場合' do
+  #       it 'コメントの編集画面に遷移できること' do
+  #       end
+  #     end
+  #     context 'ユーザーが自分ではない場合' do
+  #       it 'コメントの編集画面に遷移できないこと' do
+  #       end
+  #     end
+  #   end
+  #   context 'ログインしていないとき' do
+  #     it 'コメントの編集画面に遷移できないこと' do
+  #     end
+  #   end
+  # end
 
-    #   before 'ユーザーIDをセッションから取り出せるようにする' do
-    #     allow_any_instance_of(ActionDispatch::Request)
-    #       .to receive(:session).and_return(user_id: user.id)
-    #     get edit_user_path(user.id)
-    #   end
-    #   it 'リクエストが成功すること' do
-    #     expect(response.status).to eq 200
-    #   end
-    #   it 'ユーザー名が表示されていること' do
-    #     expect(response.body).to include user.name
-    #   end
-    # end
-  end
+  # describe 'GET #show' do
+  #   context 'ログインしているとき' do
+  #     it 'コメントの詳細画面に遷移できること' do
+  #     end
+  #   end
+  #   context 'ログインしていないとき' do
+  #     it 'コメントの詳細画面に遷移できないこと' do
+  #     end
+  #   end
+  # end
 
-  describe 'GET #show' do
-    context 'ログインしているとき' do
-      it 'コメントの詳細画面に遷移できること' do
-      end
-    end
-    context 'ログインしていないとき' do
-      it 'コメントの詳細画面に遷移できないこと' do
-      end
-    end
-  end
-
-  describe 'PUT #update' do
-    context 'ログインしていないとき' do
-      it 'コメント情報が更新されないこと' do
-      end
-    end
-  end
+  # describe 'PUT #update' do
+  #   context 'ログインしていないとき' do
+  #     it 'コメント情報が更新されないこと' do
+  #     end
+  #   end
+  # end
 
   describe 'DELETE #destroy' do
     context 'ログインしているとき' do
