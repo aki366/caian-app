0a4cd943fee729df68b796d27bc2f3174405c2ac
aki366 <96239914+aki366@users.noreply.github.com>
2022-06-27 21:00:55 +0900

    0627_test_'DELETE #destroy'_'ログインしていないとき'_'コメントの削除ができないこと' #82

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -61,22 +61,22 @@ RSpec.describe "Comments Request", type: :request do
 
   describe 'DELETE #destroy' do
     subject { delete "/posts/#{user_post.id}/comments/#{post_comment.id}" }
+    # テスト条件
+    # user.id(1)の投稿に対して、user.id(2)がコメントを投稿した状態
     let!(:user_post) { create(:post) }
     let!(:user) { create(:user) }
+    let!(:post_comment) { create(:comment, post_id: user_post.id, user_id: user.id) }
     let(:other_user) { create(:user) }
-    let(:post_comment) { create(:comment, post_id: user_post.id, user_id: user.id) }
-    include_context 'login_as_user'
     context 'ログインしているとき' do
+      include_context 'login_as_user'
       context 'ユーザーが自分の場合' do
         it 'コメントの削除ができること' do
-          post_comment
           expect { subject }.to change(Comment, :count).by(-1)
           expect(response).to have_http_status(:redirect)
         end
       end
       context 'ユーザーが自分ではない場合' do
         it 'コメントの削除ができないこと' do
-          post_comment
           other_user
           expect { subject }.not_to change { post_comment }
           expect(response).to have_http_status(:redirect)
@@ -85,7 +85,8 @@ RSpec.describe "Comments Request", type: :request do
     end
     context 'ログインしていないとき' do
       it 'コメントの削除ができないこと' do
-        # byebug
+        expect { subject }.not_to change { post_comment }
+        expect(response).to have_http_status(:redirect)
       end
     end
   end
