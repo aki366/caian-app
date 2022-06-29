a7d7f607823776d286be0f214d226a9151125127
aki366 <96239914+aki366@users.noreply.github.com>
2022-06-29 23:51:57 +0900

    0629_test_POST #create_ログインしているとき_イイねが作成できること #90

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 RSpec.describe "Likes Request", type: :request do
 
   describe 'POST #create' do
-    context 'パラメータが正常なとき' do
+    subject { post post_likes_path(user_post.id) }
+    let!(:user_post) { create(:post) }
+    let!(:user) { create(:user) }
+    context 'ログインしているとき' do
+      include_context 'login_as_user'
       it 'イイね!が作成できること' do
+        expect { subject }.to change(Like, :count).by(+1)
+        expect(response).to have_http_status(:redirect)
       end
     end
-    context 'パラメータが不正なとき' do
+    context 'ログインしていないとき' do
       it 'イイね!が作成できないこと' do
       end
     end
