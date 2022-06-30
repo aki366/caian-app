b45c27936c7b33b0678e36db795786632236f52d
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-01 05:33:49 +0900

    0701_test_GET #top_トップ画面に遷移できること #89

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Home Request', type: :request do
 
   describe 'GET #top' do
+    subject { get top_path }
     it 'トップ画面に遷移できること' do
+      subject
+      expect(response).to be_successful
     end
   end
 
