d6cf1206162771268e7a8d6a7a32a8dd637427d7
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-01 05:29:13 +0900

    0701_test_テストケース作成 #89

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Home Request', type: :request do
+
+  describe 'GET #top' do
+    it 'トップ画面に遷移できること' do
+    end
+  end
+
+  describe 'GET #about' do
+    it 'アバウト画面に遷移できること' do
+    end
+  end
+end
