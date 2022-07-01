c938d63f3880e2ba32e84f824ce6ff120d2882cf
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-01 23:55:52 +0900

    0701_test_POST #create_ログインしているとき_パラメータが正常な場合_トークルームが作成できること #114

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -3,8 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Rooms Request', type: :request do
 
   describe 'POST #create' do
-    context 'パラメータが正常なとき' do
-      it 'トークルームを作成できること' do
+    subject { post rooms_path(user_ids: [user.id, room_user.id]) }
+    let!(:user) { create(:user) }
+    let(:room_user) { create(:user) }
+    context 'ログインしているとき' do
+      include_context 'login_as_user'
+      context 'パラメータが正常な場合' do
+        it 'トークルームが作成できること' do
+          expect { subject }.to change(RoomUser, :count).by(+2)
+          expect(response).to have_http_status(:redirect)
+        end
       end
     end
   end
