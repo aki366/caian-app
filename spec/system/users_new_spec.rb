7a954997c8b6b9b3de02b7394502f6f7cd5d2d26
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-03 23:08:02 +0900

    0703_test_system/users_new_spec.rb_テストシナリオ作成 #155

--- spec/system/users_new_spec.rb
+++ spec/system/users_new_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'UsersNew', type: :system do
 
+  describe 'ユーザーの新規登録をするとき' do
+    context 'フォームの入力値が正常な場合' do
+      it 'ユーザーが作成できること' do
+        # 新規ユーザー登録画面にアクセス
+        # フォームへテキストを入力
+        # 新規登録ボタンをクリックするとユーザーが作成される
+        # ユーザー詳細画面へ遷移する
+        # メッセージが表示される
+      end
+    end
+    context 'メールアドレスが未入力の場合' do
+      it 'ユーザーが作成できないこと' do
+      end
+    end
+    context '登録済のメールアドレスを使用した場合' do
+      it 'ユーザーが作成できないこと' do
+      end
+    end
+  end
 end
