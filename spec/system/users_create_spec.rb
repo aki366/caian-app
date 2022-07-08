fb0b017b33cb0b0cd17a32a45894a3d53b730879
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-08 10:41:05 +0900

    0708_test_メールアドレスが未入力の場合_ユーザーが作成できないこと #155

--- spec/system/users_create_spec.rb
+++ spec/system/users_create_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'UsersNew', type: :system do
+RSpec.describe 'Users #create system', type: :system do
 
   describe 'ユーザーの新規登録をするとき' do
     context 'フォームの入力値が正常な場合' do
@@ -12,7 +12,7 @@ RSpec.describe 'UsersNew', type: :system do
         fill_in 'user_email',    with: 'system@example.com'
         fill_in 'user_password', with: 'password'
         # 新規登録ボタンをクリックするとユーザーが作成される
-        expect { click_button '新規登録' }.to change { User.count }.by(+1)
+        expect { click_on '新規登録' }.to change { User.count }.by(+1)
         # ユーザー詳細画面へ遷移する
         expect(users_path).to eq users_path
         # メッセージが表示される
@@ -25,15 +25,14 @@ RSpec.describe 'UsersNew', type: :system do
         visit new_user_path
         # メールフォームの値のみnil
         fill_in 'user_name',     with: 'system_山田'
-        fill_in 'user_email',    with: ''
         fill_in 'user_password', with: 'password'
+        fill_in 'user_email',    with: ''
         # 新規登録ボタンをクリックしてもユーザーが作成されない
-        expect { click_button '新規登録' }.to change { User.count }.by(+0)
-        # 新規ユーザー登録画面にリダイレクトされる
-        expect(users_path).to eq new_user_path
+        expect { click_on '新規登録' }.to change { User.count }.by(+0)
         # メッセージが表示される
-        expect(user.errors.full_messages).to include('Emailを入力してください')
-        # expect(page).to have_content 'Emailを入力してください'
+        expect(page).to have_content 'Emailを入力してください'
+        # 新規ユーザー登録画面にリダイレクトされる
+        visit new_user_path
       end
     end
     context '登録済のメールアドレスを使用した場合' do
