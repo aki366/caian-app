54406b8ee0069b5239ab5a8c6449a918e338e0fa
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-08 21:23:47 +0900

    0708_test_パスワードが未入力の場合_ユーザー情報が更新されること #161

--- spec/system/users_update_spec.rb
+++ spec/system/users_update_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Users #update system', type: :system do
         # ユーザー情報の編集画面にアクセス
         visit edit_user_path(user.id)
         # フォームへ正常な値を入力
-        fill_in 'name', with: 'system_山田'
+        fill_in 'name', with: 'ヤマダ'
         # 変更ボタンをクリックするとユーザー情報が編集される
         expect { click_on '変更' }.to change { User.last.name }
         # 現在のパスが指定されたパスであることを検証する
@@ -68,7 +68,6 @@ RSpec.describe 'Users #update system', type: :system do
         # フォームへ正常な値を入力
         fill_in 'password', with: 'password'
         # 変更ボタンをクリックするとユーザー情報が編集される
-        # byebug
         expect { click_on '変更' }.to change { User.last.password_digest }
         # 現在のパスが指定されたパスであることを検証する
         expect(current_path).to eq user_path(user.id)
@@ -76,8 +75,19 @@ RSpec.describe 'Users #update system', type: :system do
         expect(page).to have_content 'ユーザー情報を編集しました'
       end
     end
-    context 'パスワードが未入力の場合でも' do
+    context 'パスワードが未入力の場合' do
       it 'ユーザー情報が更新されること' do
+        # ユーザー情報の編集画面にアクセス
+        visit edit_user_path(user.id)
+        # フォームへ正常な値を入力
+        fill_in 'name',     with: 'ヤマダ'
+        fill_in 'password', with: ''
+        # 変更ボタンをクリックするとユーザー情報が編集される
+        expect { click_on '変更' }.to change { User.last.name }
+        # 現在のパスが指定されたパスであることを検証する
+        expect(current_path).to eq user_path(user.id)
+        # メッセージが表示される
+        expect(page).to have_content 'ユーザー情報を編集しました'
       end
     end
   end
