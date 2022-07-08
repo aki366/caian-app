6f16f4d9e1b93ab413843a943f6f8550c171682f
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-08 16:34:47 +0900

    0708_test_名前が未入力の場合_ユーザー情報が更新されないこと #161

--- spec/system/users_update_spec.rb
+++ spec/system/users_update_spec.rb
@@ -19,6 +19,20 @@ RSpec.describe 'Users #update system', type: :system do
         expect(page).to have_content 'ユーザー情報を編集しました'
       end
     end
+    context '名前が未入力の場合' do
+      it 'ユーザー情報が更新されないこと' do
+        # ユーザー情報の編集画面にアクセス
+        visit edit_user_path(user.id)
+        # 名前がnil
+        fill_in 'name', with: ''
+        # 変更ボタンをクリックしてもユーザー情報が編集されない
+        expect { click_on '変更' }.not_to change { User.last.name }
+        # 現在のパスが指定されたパスであることを検証する
+        expect(current_path).to eq "/users/#{user.id}"
+        # エラーメッセージが表示されること
+        expect(page).to have_content 'Nameを入力してください'
+      end
+    end
     context 'メールアドレスを変更した場合' do
       it 'ユーザー情報が更新されること' do
         # ユーザー情報の編集画面にアクセス
