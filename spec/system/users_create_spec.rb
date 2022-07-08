bcee17c1acb3531bd7679849a3b9ebc532a0826f
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-08 11:03:56 +0900

    0708_test_登録済のメールアドレスを使用した場合_ユーザーが作成できないこと #155

--- spec/system/users_create_spec.rb
+++ spec/system/users_create_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Users #create system', type: :system do
         fill_in 'user_email',    with: 'system@example.com'
         fill_in 'user_password', with: 'password'
         # 新規登録ボタンをクリックするとユーザーが作成される
-        expect { click_on '新規登録' }.to change { User.count }.by(+1)
+        expect { click_on '新規登録' }.to change { User.count }.by(1)
         # ユーザー詳細画面へ遷移する
         expect(users_path).to eq users_path
         # メッセージが表示される
@@ -25,23 +25,31 @@ RSpec.describe 'Users #create system', type: :system do
         visit new_user_path
         # メールフォームの値のみnil
         fill_in 'user_name',     with: 'system_山田'
-        fill_in 'user_password', with: 'password'
         fill_in 'user_email',    with: ''
+        fill_in 'user_password', with: 'password'
         # 新規登録ボタンをクリックしてもユーザーが作成されない
-        expect { click_on '新規登録' }.to change { User.count }.by(+0)
-        # メッセージが表示される
+        expect { click_on '新規登録' }.to change { User.count }.by(0)
+        # エラーメッセージが表示されること
         expect(page).to have_content 'Emailを入力してください'
         # 新規ユーザー登録画面にリダイレクトされる
         visit new_user_path
       end
     end
     context '登録済のメールアドレスを使用した場合' do
+      let(:user) { create(:user) }
       it 'ユーザーが作成できないこと' do
         # 新規ユーザー登録画面にアクセス
+        visit new_user_path
         # メールフォームに登録済のメールアドレスを入力
+        fill_in 'user_name',     with: 'system_山田'
+        fill_in 'user_email',    with: user.email
+        fill_in 'user_password', with: 'password'
         # 新規登録ボタンをクリックしてもユーザーが作成されない
+        expect { click_on '新規登録' }.to change { User.count }.by(0)
+        # エラーメッセージが表示されること
+        expect(page).to have_content 'Emailはすでに存在します'
         # 新規ユーザー登録画面にリダイレクトされる
-        # メッセージが表示される
+        visit new_user_path
       end
     end
   end
