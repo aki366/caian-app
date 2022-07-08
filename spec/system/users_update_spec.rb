29725a1d6a0a900e383d652d0aec9c9a95971958
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-08 15:23:32 +0900

    0708_test_メールアドレスを変更した場合_ユーザー情報が更新されること #161

--- spec/system/users_update_spec.rb
+++ spec/system/users_update_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Users #update system', type: :system do
   describe 'ログインしているとき' do
     let!(:user) { create(:user) }
     include_context 'login_as_user'
-    context '名前が変更された場合' do
+    context '名前を変更した場合' do
       it 'ユーザー情報が更新されること' do
         # ユーザー情報の編集画面にアクセス
         visit edit_user_path(user.id)
@@ -13,6 +13,20 @@ RSpec.describe 'Users #update system', type: :system do
         fill_in 'name',     with: 'system_山田'
         # 変更ボタンをクリックするとユーザー情報が編集される
         expect { click_on '変更' }.to change { User.last.name }
+        # ユーザー詳細画面へ遷移する
+        expect(user_path(user.id)).to eq user_path(user.id)
+        # メッセージが表示される
+        expect(page).to have_content 'ユーザー情報を編集しました'
+      end
+    end
+    context 'メールアドレスを変更した場合' do
+      it 'ユーザー情報が更新されること' do
+        # ユーザー情報の編集画面にアクセス
+        visit edit_user_path(user.id)
+        # フォームへ正常な値を入力
+        fill_in 'email',     with: 'system@example.com'
+        # 変更ボタンをクリックするとユーザー情報が編集される
+        expect { click_on '変更' }.to change { User.last.email }
         # ユーザー詳細画面へ遷移する
         expect(user_path(user.id)).to eq user_path(user.id)
         # メッセージが表示される
