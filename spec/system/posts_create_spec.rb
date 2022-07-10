5905995a2c0aaa2efa069e505c51237399811db3
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-10 17:53:10 +0900

    0710_test_投稿内容が未入力の場合_投稿が作成できないこと #179

--- spec/system/posts_create_spec.rb
+++ spec/system/posts_create_spec.rb
@@ -21,5 +21,25 @@ RSpec.describe 'Posts #create system', type: :system do
         expect(page).to have_content '投稿を作成しました'
       end
     end
+    context '投稿内容が未入力の場合' do
+      it '投稿が作成できないこと' do
+        # トップ画面にアクセス
+        visit root_path
+        # 新規投稿をクリック
+        click_on "新規投稿"
+        # 投稿フォームの値がnil
+        fill_in 'post_content',     with: ''
+        # "投稿する"をクリックしても投稿が作成されない
+        expect { click_on '投稿する' }.to change { Post.count }.by(0)
+        # 現在のパスが指定されたパスであることを検証する
+        expect(current_path).to eq "/posts"
+        # メッセージが表示される
+        expect(page).to have_content 'Contentを入力してください'
+      end
+    end
+    context '投稿内容が1001文字の場合' do
+      it '投稿が作成できないこと' do
+      end
+    end
   end
 end
