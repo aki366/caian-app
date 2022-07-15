801978f22f967e6164096ff9a502e7661790e302
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-15 12:34:17 +0900

    0715_messagess_column_rename_app/models/message.rb_修正 #196

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  validates :message_text, {presence: true, length: {maximum: 1000}}
+  validates :text, {presence: true, length: {maximum: 1000}}
 
   belongs_to :room
   belongs_to :user
