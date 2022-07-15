c947bdfbb641de427bd870a9c712c56d2fbc9d05
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-15 12:03:29 +0900

    0715_commentテーブルのカラム名変更後のマイグレーション #195

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_21_070007) do
+ActiveRecord::Schema.define(version: 2022_07_15_025653) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2022_05_21_070007) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "comment_text"
+    t.text "text"
     t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
