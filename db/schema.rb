66872f82f6c4ca0b7d2a93ae43799c1116c81a7a
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-16 16:06:17 +0900

    0716_comments_table_rename_column_post_id_ticket_id_マイグレーション #194

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_051731) do
+ActiveRecord::Schema.define(version: 2022_07_16_055646) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,10 +43,10 @@ ActiveRecord::Schema.define(version: 2022_07_15_051731) do
   create_table "comments", force: :cascade do |t|
     t.text "text"
     t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.integer "ticket_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["ticket_id"], name: "index_comments_on_ticket_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 2022_07_15_051731) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "tickets", column: "post_id"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "tickets", column: "post_id"
   add_foreign_key "likes", "users"
