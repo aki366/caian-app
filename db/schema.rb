0d1b017d1079975fd8d7f3e84fa1e04a7ca56d5d
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-16 22:42:53 +0900

    0716_users_careerカラム削除 #203

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_133138) do
+ActiveRecord::Schema.define(version: 2022_07_16_134132) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 2022_07_16_133138) do
     t.string "profile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "career"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
