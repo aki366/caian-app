d84d384a1a8a750133e20ecf9e4b50ad60bf5f05
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-18 00:55:02 +0900

    0717_new_column_tickets_subject_team_id_カラム追加_マイグレーション #59

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_142350) do
+ActiveRecord::Schema.define(version: 2022_07_17_155110) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -102,6 +102,8 @@ ActiveRecord::Schema.define(version: 2022_07_17_142350) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "subject"
+    t.integer "team_id"
   end
 
   create_table "users", force: :cascade do |t|
