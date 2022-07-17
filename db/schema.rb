ed437da0f6a1eab08bc78ec4a051382da8e00061
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-17 23:33:40 +0900

    0717_new_teams_table_チームテーブル新規作成 #59

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_134809) do
+ActiveRecord::Schema.define(version: 2022_07_17_142350) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,12 @@ ActiveRecord::Schema.define(version: 2022_07_16_134809) do
   end
 
   create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
