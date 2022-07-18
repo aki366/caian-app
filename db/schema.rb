07af2f83d2479a928b8482012581ef23a2058d77
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-19 00:58:36 +0900

    0719_add_index_foreign_key_members_マイグレーション #59

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_17_162248) do
+ActiveRecord::Schema.define(version: 2022_07_18_155314) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2022_07_17_162248) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_members_on_team_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -131,6 +133,8 @@ ActiveRecord::Schema.define(version: 2022_07_17_162248) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "tickets"
   add_foreign_key "likes", "users"
+  add_foreign_key "members", "teams"
+  add_foreign_key "members", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
