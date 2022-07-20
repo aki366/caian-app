cffbba38b5a977d61e80bd7d7ad8110134866a32
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-21 08:56:50 +0900

    0721_team新規作成時のエラーメッセージ出力 #213

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,21 +4,24 @@ class TeamsController < ApplicationController
 
   def create
     @team = Team.new(team_params)
-    @team.save
-    flash[:notice] = "チームを作成しました"
-    redirect_to new_team_path
-  end
-
-  def show
-    @team = Team.find(params[:id])
-    @message = Message.new
-    if Member.where(:user_id => @current_user.id, :team_id => @team.id).present?
-      @messages = @team.team_users
-      @team_users = @team.messages.includes(:user)
+    if @team.save
+      flash[:notice] = "チームを作成しました"
+      redirect_to new_team_path
     else
-      redirect_back(fallback_location: root_path)
+      render :new
     end
   end
+
+  # def show
+  #   @team = Team.find(params[:id])
+  #   @message = Message.new
+  #   if Member.where(:user_id => @current_user.id, :team_id => @team.id).present?
+  #     @messages = @team.team_users
+  #     @team_users = @team.messages.includes(:user)
+  #   else
+  #     redirect_back(fallback_location: root_path)
+  #   end
+  # end
 
   def new
     @team = Team.new
