3a7c4abd252f100c463d61d3ddba091521796c16
aki366 <96239914+aki366@users.noreply.github.com>
2022-07-21 16:16:35 +0900

    0721_models/team.rb_アソシエーション設定 #219

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,7 @@ class Team < ApplicationRecord
 
   validates :name, {presence: true, length: {maximum: 50}}
 
-  # has_many :members, dependent: :destroy
-  # has_many :tickets, dependent: :destroy
-  # has_many :users, through: :members, dependent: :destroy
-
+  has_many :members, dependent: :destroy
+  has_many :tickets, dependent: :destroy
+  has_many :users, through: :members, dependent: :destroy
 end
