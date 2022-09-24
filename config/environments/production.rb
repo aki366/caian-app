b01de6b9fe28d9d3606651810d6ebacfcdb5cddc
aki366 <96239914+aki366@users.noreply.github.com>
2022-09-25 07:12:26 +0900

    0925_AWS_ECS_on_Fargate_サービス公開成功 #261

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # 接続先のホスト名を設定
+  config.hosts << "caian.link"
+
 end
