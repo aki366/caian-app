1564fb96a2b926d17b61d04f8a085188f1b357dc
aki366 <96239914+aki366@users.noreply.github.com>
2023-08-10 23:52:12 +0900

    実質使っていないヘルスチェックを削除し、メッセージの文言修正

--- bin/setup_docker.sh
+++ bin/setup_docker.sh
@@ -2,14 +2,12 @@
 
 echo -e "start caian_app setup_docker\n"
 
-# イメージ名とタグを指定
 IMAGE_NAME="caian_app"
-# caian_app イメージの作成日を取得
 CREATION_DATE=$(docker inspect $IMAGE_NAME --format '{{.Created}}' 2>/dev/null)
 
-echo "コンテナイメージの状態をチェックします。"
+echo "コンテナイメージの確認"
 if [ -z "$CREATION_DATE" ]; then
-  echo -e " $IMAGE_NAME イメージが存在しないため、イメージを作成します。\n"
+  echo " $IMAGE_NAME のイメージが存在していないため、作成しています。"
   docker compose -f docker-compose.yml build
   if [[ $? != 0 ]]; then
     echo -e " Dockerイメージのビルドに失敗しました。\n"
@@ -35,48 +33,40 @@ else
       echo -e " Dockerイメージのビルドに失敗しました。\n"
       exit 1
     fi
+  else
+    echo -e " 古いイメージではないため、更新しません。\n"
   fi
-  echo -e " 古いイメージではないため、更新しません。\n"
 fi
 
-# Dockerコンテナを起動
-echo "コンテナを起動します。"
+echo -e "\nコンテナの起動"
 docker compose -f docker-compose.yml up -d
-echo
 
-# caian_appのセットアップ
-echo "caian_appのセットアップを開始します。"
+echo -e "\nDBのセットアップ確認"
 
-start_time=$(date +%s)
+# DB用コンテナの起動状態を確立する
+if ! docker compose -f docker-compose.yml logs mysql 2>&1 | grep -q "Ready for start up"; then
+  start_time=$(date +%s)
+  echo " Waiting for MySQL to be ready..."
 
-# MySQLのログに「Ready for start up」が出力されるのを待つ
-echo " Waiting for MySQL to be ready..."
-while ! docker compose -f docker-compose.yml logs mysql 2>&1 | grep -q "Ready for start up"; do
-  elapsed_time=$(($(date +%s) - start_time))
-  echo -ne "  Elapsed time: $elapsed_time sec.\r"
-  sleep 1
-done
-
-echo -e "\n  MySQL is now ready. Execution time: $elapsed_time sec.\n"
+  while ! docker compose -f docker-compose.yml logs mysql 2>&1 | grep -q "Ready for start up"; do
+    elapsed_time=$(($(date +%s) - start_time))
+    echo -ne "  Elapsed time: $elapsed_time sec.\r"
+    sleep 1
+  done
+  echo -e "\n  MySQL is now ready.\n"
+fi
 
 # caian-app-1が起動していない場合、コンテナを起動する
 if [ "$(docker inspect --format '{{.State.Running}}' caian-app-1)" == "false" ]; then
-  docker start caian-app-1
+  echo " caian-app コンテナが停止しているため、起動します。"
+  docker start caian-app-1 > /dev/null 2>&1
 fi
 
-# caian-app-1のヘルスチェックがtrueになるのを待つ
-until [ "$(docker inspect --format '{{.State.Status}}' caian-app-1)" == "running" ]; do
-  echo "Waiting for caian-app-1 to be healthy..."
-  sleep 2
-done
-
-# コンテナ内でのMySQLのデータベース一覧を取得
+# データベース一覧を取得
 DATABASES=$(docker-compose -f docker-compose.yml exec -T mysql mysql -u root -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
-echo " DBのセットアップを開始します。"
-# caian_appデータベースの存在確認
+# 開発環境用DBの確認
 if ! echo " $DATABASES" | grep -q "caian_development"; then
-  # caian_appが存在しない場合、データベースを作成
   echo "  caian_development が存在していないため、作成しています。"
   docker compose -f docker-compose.yml exec -T app bundle exec rails db:create > /dev/null 2>&1
   docker compose -f docker-compose.yml exec -T app bundle exec rails db:migrate > /dev/null 2>&1
@@ -85,9 +75,8 @@ else
   echo -e "  caian_development は存在していたため、作成しません。"
 fi
 
-# caian_testデータベースの存在確認
+# テスト用DBの確認
 if ! echo "$DATABASES" | grep -q "caian_test"; then
-  # caian_testが存在しない場合、データベースを作成しマイグレーションを実行
   echo "  caian_test が存在していないため、作成しています。"
   docker compose -f docker-compose.yml exec -T app bundle exec rails db:create RAILS_ENV=test > /dev/null 2>&1
   docker compose -f docker-compose.yml exec -T app bundle exec rails db:migrate RAILS_ENV=test > /dev/null 2>&1
@@ -96,6 +85,5 @@ else
   echo -e "  caian_test は存在していたため、作成しません。\n"
 fi
 
-# コンテナの状態を表示
 echo -e "セットアップが完了しました。\n"
 docker ps -a --format "{{.ID}} {{.Names}} {{.Status}}" | awk '{ printf "%-13s %-44s %-50s\n", $1, $2, $3 " " $4 " " $5 " " $6 " " $7 " " $8 " " $9}'
