c34659ef96cfc54bdeca2e861c3a5603d0fb1878
aki366 <96239914+aki366@users.noreply.github.com>
2023-08-10 14:53:13 +0900

    appコンテナのdbを作成処理を追加

--- bin/setup_docker.sh
+++ bin/setup_docker.sh
@@ -40,3 +40,29 @@ fi
 # Dockerコンテナを起動
 docker compose -f docker-compose.yml up -d
 echo -e "コンテナを起動しました\n"
+
+# caian_appのセットアップ
+echo "caian_appのセットアップを開始します。"
+
+# コンテナ内でのMySQLのデータベース一覧を取得
+DATABASES=$(docker-compose -f docker-compose.yml exec -T mysql mysql -u root -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
+
+# caian_appデータベースの存在確認
+if ! echo "$DATABASES" | grep -q "caian_development"; then
+  # caian_appが存在しない場合、データベースを作成
+  docker compose -f docker-compose.yml exec -T app bundle exec rails db:create
+  docker compose -f docker-compose.yml exec -T app bundle exec rails db:migrate
+  echo -e "caian_development を作成しました。"
+else
+  echo -e "caian_development は存在していたため、作成しません。"
+fi
+
+# caian_testデータベースの存在確認
+if ! echo "$DATABASES" | grep -q "caian_test"; then
+  # caian_testが存在しない場合、データベースを作成しマイグレーションを実行
+  docker compose -f docker-compose.yml exec -T app bundle exec rails db:create RAILS_ENV=test
+  docker compose -f docker-compose.yml exec -T app bundle exec rails db:migrate RAILS_ENV=test
+  echo -e "caian_test を作成しました。\n"
+else
+  echo -e "caian_test は存在していたため、作成しません。\n"
+fi
