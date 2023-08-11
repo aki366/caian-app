3c4f7cfd19fc63009d656ff46c842ee94e79491d
aki366 <96239914+aki366@users.noreply.github.com>
2023-08-11 16:33:02 +0900

    entrypoint.shをsetup_docker.shから呼び出して実行するように修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /caian_app/tmp/pids/server.pid
 
+echo "開発用DBの作成"
 bundle exec rails db:create
 bundle exec rails db:migrate
 
+echo -e "\nテスト用DBの作成"
 bundle exec rails db:create RAILS_ENV=test
 bundle exec rails db:migrate RAILS_ENV=test
 
