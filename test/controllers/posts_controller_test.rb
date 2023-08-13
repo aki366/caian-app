3f49bea8c115e6382a8c826f2ae2e0fdc481ff02
aki366 <96239914+aki366@users.noreply.github.com>
2023-08-14 02:10:12 +0900

    testディレクトリは削除

--- test/controllers/posts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TicketsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tickets_index_url
-    assert_response :success
-  end
-end
