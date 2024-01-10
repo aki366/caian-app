d9ed26e3100502a2d65b5d797676b39c3b224d67
aki366 <akihiro.okuda366@gmail.com>
2024-01-11 00:00:31 +0900

    team_messages_controllerのlintエラーの修正

--- apps/backend/app/controllers/team_messages_controller.rb
+++ apps/backend/app/controllers/team_messages_controller.rb
@@ -1,5 +1,4 @@
 class TeamMessagesController < ApplicationController
-
   before_action :authenticate_user
 
   def create
@@ -7,19 +6,18 @@ class TeamMessagesController < ApplicationController
     @team_message = TeamMessage.new(team_message_params)
     @team_message.user_id = @current_user.id
     @team_message.team_id = @team.id
-    if @team_message.save
-      redirect_to room_team_path(@team.id)
-    end
+    redirect_to room_team_path(@team.id) if @team_message.save
   end
 
   def destroy
     @team = Team.find(params[:team_id])
     @team_message = @team.team_messages.find(params[:id])
-    if @current_user.id == @team_message.user.id
-      @team_message.destroy
-      flash[:notice] = t('flash_messages.message_deleted')
-      redirect_to room_team_path(@team.id)
-    end
+
+    return unless @current_user.id == @team_message.user.id
+
+    @team_message.destroy
+    flash[:notice] = t('flash_messages.message_deleted')
+    redirect_to room_team_path(@team.id)
   end
 
   private
