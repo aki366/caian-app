5cd4d8d235c48d271d5fdaa4a5ac5c4316466e5c
aki366 <akihiro.okuda366@gmail.com>
2025-02-09 03:01:00 +0900

    update: テストの検証をbe_validに修正、ticket_spec

--- apps/backend/spec/models/ticket_spec.rb
+++ apps/backend/spec/models/ticket_spec.rb
@@ -1,24 +1,23 @@
 RSpec.describe Ticket, type: :model do
-  let!(:user) { FactoryBot.create(:user) }
+  let(:user) { FactoryBot.create(:user) }
   let(:ticket) { FactoryBot.build(:ticket, user: user, subject: "テストサブジェクト", content: content) }
 
   describe '#create' do
-    subject { ticket.valid? }
 
     describe '正常系' do
-      context 'contentが1文字のとき' do
-        let!(:content) { 'a' }
+      context 'contentに英数字、大文字小文字、記号ひらがなカタカナ漢字で投稿したとき' do
+        let(:content) { '@:20xX、新人育成のベストプラクティス１０' }
 
         it '投稿が成功すること' do
-          is_expected.to be true
+          expect(ticket).to be_valid
         end
       end
 
       context 'contentが1000文字のとき' do
-        let!(:content) { 'a' * 1000 }
+        let(:content) { 'a' * 1000 }
 
         it '投稿が成功すること' do
-          is_expected.to be true
+          expect(ticket).to be_valid
         end
       end
     end
@@ -28,16 +27,16 @@ RSpec.describe Ticket, type: :model do
         let!(:content) { '' }
 
         it '投稿が失敗すること' do
-          is_expected.to be false
+          expect(ticket).not_to be_valid
           expect(ticket.errors[:content]).to include('を入力してください')
         end
       end
 
       context 'contentが1001文字のとき' do
-        let!(:content) { 'a' * 1001 }
+        let(:content) { 'a' * 1001 }
 
         it '投稿が失敗すること' do
-          is_expected.to be false
+          expect(ticket).not_to be_valid
           expect(ticket.errors[:content]).to include('は1000文字以内で入力してください')
         end
       end
