468bd5b6f26897d0cc1cf9e85442f8a9d6f66467
aki366 <akihiro.okuda366@gmail.com>
2025-02-08 11:41:49 +0900

    update: 異常系と正常系を分け、userは最初に作成しておく

--- apps/backend/spec/models/ticket_spec.rb
+++ apps/backend/spec/models/ticket_spec.rb
@@ -1,40 +1,45 @@
 RSpec.describe Ticket, type: :model do
-  let!(:ticket) { FactoryBot.build(:ticket, subject: "テストサブジェクト", content: content) }
+  let!(:user) { FactoryBot.create(:user) }
+  let(:ticket) { FactoryBot.build(:ticket, user: user, subject: "テストサブジェクト", content: content) }
 
   describe '#create' do
     subject { ticket.valid? }
 
-    # context '投稿内容が最小の1文字のとき' do
-    #   let!(:content) { 'a' }
+    describe '正常系' do
+      context 'contentが1文字のとき' do
+        let!(:content) { 'a' }
 
-    #   it '投稿が成功すること' do
-    #     is_expected.to be true
-    #   end
-    # end
+        it '投稿が成功すること' do
+          is_expected.to be true
+        end
+      end
 
-    context '投稿内容が空のとき' do
-      let!(:content) { '' }
+      context 'contentが1000文字のとき' do
+        let!(:content) { 'a' * 1000 }
 
-      it '投稿が失敗すること' do
-        is_expected.to be false
-        expect(ticket.errors[:content]).to include('を入力してください')
+        it '投稿が成功すること' do
+          is_expected.to be true
+        end
       end
     end
 
-    # context '投稿内容が最大の1000文字のとき' do
-    #   let!(:content) { 'a' * 1000 }
+    describe '異常系' do
+      context 'contentがnullのとき' do
+        let!(:content) { '' }
 
-    #   it '投稿が成功すること' do
-    #     is_expected.to be true
-    #   end
-    # end
+        it '投稿が失敗すること' do
+          is_expected.to be false
+          expect(ticket.errors[:content]).to include('を入力してください')
+        end
+      end
 
-    context '投稿内容が1001文字のとき（上限を超える）' do
-      let!(:content) { 'a' * 1001 }
+      context 'contentが1001文字のとき' do
+        let!(:content) { 'a' * 1001 }
 
-      it '投稿が失敗すること' do
-        is_expected.to be false
-        expect(ticket.errors[:content]).to include('は1000文字以内で入力してください')
+        it '投稿が失敗すること' do
+          is_expected.to be false
+          expect(ticket.errors[:content]).to include('は1000文字以内で入力してください')
+        end
       end
     end
   end
