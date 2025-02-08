fd33ef28050f8f4552a481b38e2792cdfadcb752
aki366 <akihiro.okuda366@gmail.com>
2025-02-08 11:21:56 +0900

    clean: ticket_spec 異常ケースのリファクタ

--- apps/backend/spec/models/ticket_spec.rb
+++ apps/backend/spec/models/ticket_spec.rb
@@ -1,36 +1,40 @@
-RSpec.describe "Tickets Model", type: :model do
-  before do
-    @ticket = FactoryBot.build(:ticket)
-  end
+RSpec.describe Ticket, type: :model do
+  let!(:ticket) { FactoryBot.build(:ticket, subject: "テストサブジェクト", content: content) }
 
   describe '#create' do
-    context '投稿内容が1文字のとき' do
-      it '投稿が成功すること' do
-        @ticket.content = 'a' * 1
-        @ticket.valid?
-      end
-    end
+    subject { ticket.valid? }
 
-    context '投稿内容がないとき' do
+    # context '投稿内容が最小の1文字のとき' do
+    #   let!(:content) { 'a' }
+
+    #   it '投稿が成功すること' do
+    #     is_expected.to be true
+    #   end
+    # end
+
+    context '投稿内容が空のとき' do
+      let!(:content) { '' }
+
       it '投稿が失敗すること' do
-        @ticket.content = ''
-        @ticket.valid?
-        expect(@ticket.errors.full_messages).to include('Contentを入力してください')
+        is_expected.to be false
+        expect(ticket.errors[:content]).to include('を入力してください')
       end
     end
 
-    context '投稿内容が1000文字のとき' do
-      it '投稿が成功すること' do
-        @ticket.content = 'a' * 1000
-        @ticket.valid?
-      end
-    end
+    # context '投稿内容が最大の1000文字のとき' do
+    #   let!(:content) { 'a' * 1000 }
 
-    context '投稿内容が1001文字のとき' do
+    #   it '投稿が成功すること' do
+    #     is_expected.to be true
+    #   end
+    # end
+
+    context '投稿内容が1001文字のとき（上限を超える）' do
+      let!(:content) { 'a' * 1001 }
+
       it '投稿が失敗すること' do
-        @ticket.content = 'a' * 1001
-        @ticket.valid?
-        expect(@ticket.errors.full_messages).to include('Contentは1000文字以内で入力してください')
+        is_expected.to be false
+        expect(ticket.errors[:content]).to include('は1000文字以内で入力してください')
       end
     end
   end
